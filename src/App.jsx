633d679e9a2703fd24680cf81807d7194550b5a0
prajwalshettar43 <prajwalshettar43@gmail.com>
2025-11-08 21:30:31 +0530

    Creation of holographic cube

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import { useState } from 'react'
-
+import HolographicCubeHero from './components/HolographicCubeHero'
 function App() {
   
   return (
-    <>
-      <p className='text-red-950'>Hi!!</p>
-    </>
+    <div className='h-screen'>
+      {/* <iframe src='https://my.spline.design/draganddropbookpencilschoolcopy-4ymnJwHrbZo6wVU762NKi71H/' frameborder='0' width='100%' height='100%'></iframe> */}
+    <HolographicCubeHero/>
+    </div>
   )
 }
 
